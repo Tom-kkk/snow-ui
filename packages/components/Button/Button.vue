31bb3163f8cf12fcceba9af8b8e040948d8b4e40
Tom-kkk <32155764@qq.com>
2025-06-05 18:02:55 +0800

    feat:添加components和button

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,12 @@
+//  Button 目录 Button.vue
+<template>
+  <button style="color: red">this is a button</button>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "SnowButton",
+});
+</script>
+
+<style scoped></style>
---
+++ packages/components/Button/index.ts
@@ -0,0 +1,5 @@
+// Button 目录 index.ts
+import Button from "./Button.vue";
+import { withInstall } from "@snow-ui/utils";
+
+export const SnowButton = withInstall(Button);
