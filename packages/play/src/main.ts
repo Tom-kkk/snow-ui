b0dd1626442defc42f1e9465ff29a81e2f0e42dd
Tom-kkk <32155764@qq.com>
2025-06-07 08:52:39 +0800

    feat:在vue中使用了snowbutton，成功使用

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import SnowButton from "snow-ui";
+
+const app = createApp(App)
+
+app.use(SnowButton)
+app.mount("#app");
+
